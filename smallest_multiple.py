6fcfb34901420da73d5805edd5878c7f55e6d724
Oliver_Ravnholdt <omrlode@gmail.com>
2024-09-14 15:20:56 +0200

    Solved problem 5 by brute forcing

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -1,8 +1,23 @@
 def problem_5():
     PROBLEM_ID = 5
 
-    return PROBLEM_ID, 5
+    num = 1
+
+    the_range = [1, 20]
+
+    while True:
+        yes = True
+        for i in range(the_range[0], the_range[1]):
+            if num % i != 0:
+                num += 1
+                yes = False
+                break
+        if yes:
+            break
+
+    return PROBLEM_ID, num
 
 
 if __name__ == "__main__":
     problem_id, something = problem_5()
+    print("The difference of sums is:", something)
