a61c489b54b1023adc63ba9bb1c9dfc30a0586a2
Oliver_Ravnholdt <omrlode@gmail.com>
2024-09-14 14:58:21 +0200

    Solve problem 4

--- sum-square-difference.py
+++ sum-square-difference.py
@@ -1,8 +1,26 @@
 def problem_4():
     PROBLEM_ID = 4
 
-    return PROBLEM_ID, 5
+    N = 100     # Amount to sum for
+
+    # Placeholder values
+    sum_of_squares = 0
+    square_of_sum = 0
+
+    # Get sum of squares and sum of all numbers for N
+    for i in range(1, N+1):
+        sum_of_squares += i**2
+        square_of_sum += i
+
+    # Square the sum of all numbers
+    square_of_sum = square_of_sum**2
+
+    # Find difference
+    diff = square_of_sum - sum_of_squares
+
+    return PROBLEM_ID, diff
 
 
 if __name__ == "__main__":
     problem_id, something = problem_4()
+    print("The difference of sums is:", something)
