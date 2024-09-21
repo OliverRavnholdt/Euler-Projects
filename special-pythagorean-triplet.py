8acb2382ba6799f3c03f876958fe3fe8e7b534f7
Oliver_Ravnholdt <omrlode@gmail.com>
2024-09-21 18:55:52 +0200

    Solved problem 9

--- special-pythagorean-triplet.py
+++ special-pythagorean-triplet.py
@@ -1,31 +1,36 @@
 def problem_9():
     PROBLEM_ID = 9
 
-    a = 3
-    b = 4
-    c = 5
+    # Starting m and n
+    m = 2
+    n = 1
 
     while True:
-        k = 1
+        # Definitions of n and m per this link
+        # https://en.wikipedia.org/wiki/Pythagorean_triple
+        a = m ** 2 - n ** 2
+        b = 2 * m * n
+        c = m ** 2 + n ** 2
+
+        # Check if total is 1000
         if a**2 + b**2 == c**2:
-            sum = a + b + c
-            print(sum)
-            if sum == 1000:
+            total = a + b + c
+            if total == 1000:
                 break
-        a = a / k
-        b = b / k
-        c = c / k
 
-        k += 1
+        # Increment m
+        m += 1
 
-        a = a*k
-        b = b*k
-        c = c*k
+        # Iterate for n + 100 size m and then repeat for new n.
+        # This might not find the value wanted value so a tweak of the value 100 might be necessary
+        if m == n+100:
+            n += 1
+            m = n + 1
 
-
-
-    return PROBLEM_ID, a*b*c
+    product = a*b*c
+    return PROBLEM_ID, product
 
 
 if __name__ == "__main__":
     problem_id, something = problem_9()
+    print("The product abc is:", something)
