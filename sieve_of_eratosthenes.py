c5094ef4829c10ec53f36231842412a78d7bb373
Oliver_Ravnholdt <omrlode@gmail.com>
2024-10-28 16:52:50 +0100

    Added use of two different modes. Is now ready for use

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -8,14 +8,30 @@ from time import time
 '''
 
 
-def prime_sieve(n, mode='number', time_display=False):
+def prime_sieve(n, mode='number', time_display=True):
     start_time = time()
 
     N = n+1
 
+    if mode == 'number':
+        primes = number_mode(N)
+    elif mode == 'primes':
+        primes = prime_mode(N)
+    else:
+        print("Wrong mode set for sieve!")
+        primes = None
+
+    end_time = time()
+
+    if time_display:
+        print("Found primes in:" + str(end_time-start_time))
+
+    return primes
+
+
+def number_mode(N):
     primes = [True for i in range(N)]
     primes[0], primes[1] = False, False
-
 
     for i in range(int(sqrt(N))):
         if primes[i]:
@@ -23,13 +39,31 @@ def prime_sieve(n, mode='number', time_display=False):
                 primes[j] = False
 
     prime_return_list = [prime for prime, status in enumerate(primes) if status]
+    return prime_return_list
 
-    end_time = time()
 
-    if time_display:
-        print("Found primes in:" + str(end_time-start_time))
+def prime_mode(n):
+    N = n*2
+    while True:
+
+        primes = [True for i in range(N)]
+        primes[0], primes[1] = False, False
+
+        for i in range(int(sqrt(N))):
+            if primes[i]:
+                for j in range(i * i, len(primes), i):
+                    primes[j] = False
+
+        prime_return_list = [prime for prime, status in enumerate(primes) if status]
+        if len(prime_return_list) >= n:
+            prime_return_list = prime_return_list[0:n-1]
+            break
+        else:
+            N = N * 2
 
     return prime_return_list
 
 
-print(prime_sieve(1000))
+if __name__ == '__main__':
+    print(prime_sieve(1000))
+    print(prime_sieve(1000, mode='primes'))
