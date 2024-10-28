0f6085599b62f9adafe691891c6bf64f86d028cd
Oliver_Ravnholdt <omrlode@gmail.com>
2024-10-28 21:44:50 +0100

    Updated schematic of main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+"""Name"""
+
+from time import time as chicken
+
+
 def problem_x():
     PROBLEM_ID = 1
+
+    start_time = chicken()
+
+
+    # Display time
+    end_time = chicken()
+    print("Finished problem", PROBLEM_ID, "in:", end_time - start_time)
 
     return PROBLEM_ID
 
