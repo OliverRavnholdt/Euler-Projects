1b191d99fedf8dd6fb379817554c79d0cd0a7f42
Oliver_Ravnholdt <omrlode@gmail.com>
2024-10-28 16:39:32 +0100

    Return list of prime numbers

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,10 +1,13 @@
 from math import sqrt
 from time import time
 
+
 ''' Modes are:
     Primes which uses n as number of wanted primes.
     Number which uses n as a number returning all primes less or equal to N.
 '''
+
+
 def prime_sieve(n, mode='number', time_display=False):
     start_time = time()
 
@@ -13,24 +16,20 @@ def prime_sieve(n, mode='number', time_display=False):
     primes = [True for i in range(N)]
     primes[0], primes[1] = False, False
 
+
     for i in range(int(sqrt(N))):
         if primes[i]:
             for j in range(i*i, len(primes), i):
                 primes[j] = False
 
+    prime_return_list = [prime for prime, status in enumerate(primes) if status]
+
     end_time = time()
-
-    print(True)
-
-    count = 0
-    for i in range(len(primes)):
-        if primes[i]:
-            print(i)
-            count += 1
-            if count == 1000: break
 
     if time_display:
         print("Found primes in:" + str(end_time-start_time))
 
+    return prime_return_list
 
-prime_sieve(1000)
+
+print(prime_sieve(1000))
