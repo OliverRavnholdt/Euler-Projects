203d5e3f4332005f7721c85bb24db16e9b110caa
Oliver_Ravnholdt <omrlode@gmail.com>
2024-10-28 23:57:13 +0100

    commented 23

--- problem_23.py
+++ problem_23.py
@@ -12,10 +12,12 @@ def problem_23():
     sum_list = [False] * (28123+1)
     abundant_list = []
 
+    # Find all abundant numbers
     for i in range(1, len(sum_list)):
         if abundant_check(i):
             abundant_list.append(i)
 
+    # Mark all values that can be written as a sum as True
     for i in abundant_list:
         for j in abundant_list:
             if i + j < len(sum_list):
@@ -23,6 +25,7 @@ def problem_23():
             else:
                 break
 
+    # Sum all numbers that can't be written as a sum of TWO abundant numbers
     total = 0
     for num, status in enumerate(sum_list):
         if not status:
@@ -34,6 +37,7 @@ def problem_23():
     print("Finished problem", PROBLEM_ID, "in:", end_time - start_time)
 
     return PROBLEM_ID, total
+
 
 def abundant_check(num):
     # Find factors (Could be done better with prime factorization (From problem 23)
@@ -49,10 +53,9 @@ def abundant_check(num):
                 factors.add(int(num/j))
                 factors.add(j)
 
-
     return sum(factors) > num
 
 
 if __name__ == "__main__":
     problem_id, something = problem_23()
-    print(something)
+    print("Your lucky number is:", something)
