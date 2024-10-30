a68ad17a2f0d8108eef66a5582ad43b3bd6275ec
Oliver_Ravnholdt <omrlode@gmail.com>
2024-10-30 01:46:09 +0100

    Solved and commented problem 29

--- problem_29.py
+++ problem_29.py
@@ -8,10 +8,14 @@ def problem_29():
 
     start_time = chicken()
 
+    # Create holder set
     terms = set()
+
+    # Values to find terms between
     min_val = 2
     max_val = 100
 
+    # Find all terms
     for a in range(min_val, max_val+1):
         for b in range(min_val, max_val+1):
             terms.add(a**b)
