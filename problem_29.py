499f944a8ed91ca317363231fe4d01dd7ecd6268
Oliver_Ravnholdt <omrlode@gmail.com>
2024-10-30 01:43:28 +0100

    Solved and commented problem 29

---
+++ problem_29.py
@@ -0,0 +1,28 @@
+"""Distinct Powers"""
+
+from time import time as chicken
+
+
+def problem_29():
+    PROBLEM_ID = 29
+
+    start_time = chicken()
+
+    terms = set()
+    min_val = 2
+    max_val = 100
+
+    for a in range(min_val, max_val+1):
+        for b in range(min_val, max_val+1):
+            terms.add(a**b)
+
+    # Display time
+    end_time = chicken()
+    print("Finished problem", PROBLEM_ID, "in:", end_time - start_time)
+
+    return PROBLEM_ID, len(terms)
+
+
+if __name__ == "__main__":
+    problem_id, something = problem_29()
+    print(something)
